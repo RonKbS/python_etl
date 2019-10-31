d13e974f90e952e9d09a8c071a96b011a1ae1dc1
RonKbS <stephen.bamwite@gmail.com>
2019-10-31 09:52:04 +0000

    add listener class

--- live_tweets.py
+++ live_tweets.py
@@ -47,4 +47,43 @@ def connect(
 	return
 
 
+# Tweepy class to access Twitter API
+
+class Streamlistener(tweepy.StreamListener):
+
+	def on_connect(self):
+		print("You are connected to the Twitter API")
+
+	def on_error(self):
+		if status_code != 200:
+			print("error found")
+
+			# returning false disconnects the stream
+			return False
+
+	def on_data(self,data):
+		try:
+			raw_data = json.loads(data)
+
+			if 'text' in raw_data:
+				username = raw_data['user']['screen_name']
+				created_at = parser.parse(raw_data['created_at'])
+				tweet = raw_data['text']
+				retweet_count = raw_data['retweet_count']
+
+				if raw_data['place'] is not None:
+					place = raw_data['place']['country']
+					print(place)
+				else:
+					place = None
+
+				location = raw_data['user']['location']
+
+				#insert data just collected into MySQL database
+				connect(username, created_at, tweet, retweet_count, place, location)
+				print("Tweet colleted at: {} ".format(str(created_at)))
+
+		except Error as e:
+			print(e)
+
 
