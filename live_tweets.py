a2c7277bfe96c81f33da93f529d15cb017cd0879
RonKbS <stephen.bamwite@gmail.com>
2019-10-31 10:35:25 +0000

    successfully pulled tweets at this point

--- live_tweets.py
+++ live_tweets.py
@@ -3,40 +3,36 @@ from mysql.connector import Error
 import tweepy
 import json
 from dateutil import parser
-import time
 import os
 
 
-cons_k = os.environ.get('CONSUMER_KEY')
-cons_s = os.environ.get('CONSUMER_SECRET')
-acc_t = os.environ.get('ACCESS_TOKEN')
-acc_t_s = os.environ.get('ACCESS_TOKEN_SECRET')
+cons_k = os.environ.get('consumer_key')
+cons_s = os.environ.get('consumer_secret')
+acc_t = os.environ.get('access_token')
+acc_t_s = os.environ.get('access_token_secret')
 pd = os.environ.get('DB_PASSWORD')
 
+
 def connect(
-    username, created_at, tweet, retweet_count, place , location
+    username, created_at, tweet, retweet_count, place, location
 ):
 	try:
 		con = mysql.connector.connect(
-            host = 'localhost',
+            host='localhost',
             database='tweet',
             user='root',
-            password = pd,
-            charset = 'utf8'
-        )
+            password=pd,
+            charset='utf8'
+		)
 
 		if con.is_connected():
 			cursor = con.cursor()
-			query = "\
-                    INSERT INTO tweets\
-                    (username, created_at, tweet, retweet_count,place, location)\
-                    VALUES (%s, %s, %s, %s, %s, %s);
-                "
+			query = "INSERT INTO tweets (username, created_at, tweet, retweet_count,place, location) VALUES (%s, %s, %s, %s, %s, %s);"
 			cursor.execute(query, (username, created_at, tweet, retweet_count, place, location))
-            try:
-			    con.commit()
-            except Error as e:
-                print(e)
+			try:
+				con.commit()
+			except Error as e:
+				print(e)
 
 	except Error as e:
 		print(e)
@@ -79,7 +75,7 @@ class Streamlistener(tweepy.StreamListener):
 
 				location = raw_data['user']['location']
 
-				#insert data just collected into MySQL database
+				# insert data just collected into MySQL database
 				connect(username, created_at, tweet, retweet_count, place, location)
 				print("Tweet colleted at: {} ".format(str(created_at)))
 
@@ -87,3 +83,21 @@ class Streamlistener(tweepy.StreamListener):
 			print(e)
 
 
+
+
+if __name__== '__main__':
+
+	auth = tweepy.OAuthHandler(cons_k, cons_s)
+	auth.set_access_token(acc_t, acc_t_s)
+	api =tweepy.API(auth, wait_on_rate_limit=True)
+
+
+
+	# create instance of Streamlistener
+	listener = Streamlistener(api = api)
+	stream = tweepy.Stream(auth, listener = listener)
+
+
+
+	track = ['okay', 'wow', 'the', 'this']
+	stream.filter(track = track, languages = ['en'])
