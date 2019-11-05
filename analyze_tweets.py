c59e6b3927c986d49f3678578cb32d2afda62c5d
RonKbS <stephen.bamwite@gmail.com>
2019-11-05 09:27:51 +0000

    create xy points of tweet sentimentality data

--- analyze_tweets.py
+++ analyze_tweets.py
@@ -1,8 +1,11 @@
 import os
 import re
 
+from itertools import groupby
+
 import mysql.connector
 import pandas as pd
+import numpy as np
 import nltk
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,6 +14,7 @@ from mysql.connector import Error
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import plotly.express as px
 
 
 
@@ -103,6 +107,7 @@ class TweetObject():
 
 
 
+
 if __name__ == '__main__':
 
     t = TweetObject( host='localhost', database='tweet', user='root')
@@ -113,16 +118,47 @@ if __name__ == '__main__':
     data['Sentiment'] = np.array([t.sentiment(x) for x in data['clean_tweets']])
 
 	# t.word_cloud(data)
-    t.save_to_csv(data)
+    '''t.save_to_csv(data)'''
+    
+    # neg_tweets[0][0][17:]
+    dated_data = data[['date', 'clean_tweets']].to_dict('split')['data']
+    pos_tweets = [tweet for index, tweet in enumerate(dated_data) if data["Sentiment"][index] > 0]
+    neg_tweets = [tweet for index, tweet in enumerate(dated_data) if data["Sentiment"][index] < 0]
+    neu_tweets = [tweet for index, tweet in enumerate(dated_data) if data["Sentiment"][index] == 0]
 
+    tweet_ranges = []
 
-    pos_tweets = [tweet for index, tweet in enumerate(data["clean_tweets"]) if data["Sentiment"][index] > 0]
-    neg_tweets = [tweet for index, tweet in enumerate(data["clean_tweets"]) if data["Sentiment"][index] < 0]
-    neu_tweets = [tweet for index, tweet in enumerate(data["clean_tweets"]) if data["Sentiment"][index] == 0]
+    '''
+    grouped_pos_tweets = [pos_tweets[x:x+5] for x in range(0, len(pos_tweets), 5)]
+    grouped_neg_tweets = [neg_tweets[x:x+5] for x in range(0, len(neg_tweets), 5)]
+    grouped_neu_tweets = [neu_tweets[x:x+5] for x in range(0, len(neu_tweets), 5)]
+    '''
+
+    xy_pos_tweets = [[int(x[0][17:])]+x for x in pos_tweets]
+    xy_neg_tweets = [[int(x[0][17:])]+x for x in neg_tweets]
+    xy_neu_tweets = [[int(x[0][17:])]+x for x in neu_tweets]
+
+    xy_pos_tweets.sort(key=lambda x: x[0])
+    xy_neg_tweets.sort(key=lambda x: x[0])
+    xy_neu_tweets.sort(key=lambda x: x[0])
+
+    gpd_xy_pos_tweets = [list(g) for k, g in groupby(xy_pos_tweets, key=lambda x: x[0])]
+    gpd_xy_neg_tweets = [list(g) for k, g in groupby(xy_neg_tweets, key=lambda x: x[0])]
+    gpd_xy_neu_tweets = [list(g) for k, g in groupby(xy_neu_tweets, key=lambda x: x[0])]
+    
+    xy_pos = [{x[0][0]: len(x)} for x in gpd_xy_pos_tweets]
+    xy_neg = [{x[0][0]: len(x)} for x in gpd_xy_neg_tweets]
+    xy_neu = [{x[0][0]: len(x)} for x in gpd_xy_neu_tweets]
+
 
 
 	#Print results
 
+    '''
     print("percentage of positive tweets: {}%".format(100*(len(pos_tweets)/len(data['clean_tweets']))))
     print("percentage of negative tweets: {}%".format(100*(len(neg_tweets)/len(data['clean_tweets']))))
     print("percentage of neutral tweets: {}%".format(100*(len(neu_tweets)/len(data['clean_tweets']))))
+    '''
+
+    # fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
+    # fig.write_html('templates/first_figure.html', auto_open=True)
